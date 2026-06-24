6e9e73809b5766e7281180ce0bff7ba49bf3755d
AlishbaAutomation <skilldigital62@gmail.com>
2026-06-24 20:42:11 +0500

    Clean project structure

--- tests/demo/test2.spec.js
+++
@@ -1,9 +0,0 @@
-import{test,expect} from '@playwright/test'
-import{Logintest} from '../../pages/validlgn'
-test('valid login' , async({page})=>{
-const ow=new Logintest(page)
-await ow.webopen()
-await ow.validlogin('Admin' , 'admin123')
-await expect(page.getByRole('heading',{name:'Dashboard'})).toBeVisible()
-
-});
--- tests/demo/uploadfile.spec.js
+++
@@ -1,7 +0,0 @@
-import {test,expect} from '@playwright/test'
-import { uploadfile } from '../../pages/uploadpage'
-test('upload' , async({page})=>{
-    const UPLOAD=new uploadfile(page)
-    await UPLOAD.Upload()
-     await expect(page.getByRole('heading' , {name:'File Uploaded!'})).toBeVisible()
-});
